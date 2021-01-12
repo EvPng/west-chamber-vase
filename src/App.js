215ce981dafa405791e4e7fb4ef284f57eaf3a95
Shiyi Peng <43157862+EvPng@users.noreply.github.com>
2021-01-12 12:32:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import * as THREE from "three";
 import { Canvas, useLoader } from "react-three-fiber";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import { OrbitControls } from "@react-three/drei";
 import "./style.css";
 
+function Vase({ murl, turl }) {
+  const vaseMesh = useLoader(PLYLoader, "/mesh.ply");
+  return <primitive object={vaseMesh} />;
+}
+
 export default function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 0, 10] }}>
+      <Suspense fallback={null}>
+        <Vase />
+      </Suspense>
       <OrbitControls />
     </Canvas>
   );
