572cf675e57a3721508b6a0eed7f9b2204e1def1
Shiyi Peng <43157862+EvPng@users.noreply.github.com>
2021-01-13 12:02:52 -0800

    Test scene

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Suspense } from "react";
 import * as THREE from "three";
 import { Canvas, useLoader } from "react-three-fiber";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import "./style.css";
 
-function Vase({ murl, turl }) {
+function Vase() {
   const vaseMesh = useLoader(PLYLoader, "/mesh.ply");
-  return <primitive object={vaseMesh} />;
+  // const vaseTex = useTexture("/texture.png");
+  // const vaseTex = useLoader(THREE.TextureLoader, "/texture.png")
+  // return <primitive object={vaseMesh} />;
+  return (
+    <>
+    <mesh>
+      <bufferGeometry geometry={vaseMesh} scale={10, 10, 10} />
+      <meshNormalMaterial />
+    </mesh>
+    <mesh>
+      <boxBufferGeometry args={[4, 4, 4]} />
+      <meshPhongMaterial color={'hotpink'} />
+    </mesh>
+    </>
+  );
 }
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 10] }}>
+    <Canvas shadowMap camera={{ position: [5, 5, 10] }} colorManagement={true} >
+      <ambientLight intensity={1} color="#e1eced" />
+      <pointLight distance={100} position={[10, 10, 30]} intensity={2} color="white" />
       <Suspense fallback={null}>
         <Vase />
       </Suspense>
